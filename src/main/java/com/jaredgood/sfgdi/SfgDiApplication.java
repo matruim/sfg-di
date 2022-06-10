327a7567a4ff40ada3618ce989796f3414d3ff04
Jared Good <jared_danell@yahoo.com>
2022-06-10 14:05:52 -0600

    singleton vs prototype

--- src/main/java/com/jaredgood/sfgdi/SfgDiApplication.java
+++ src/main/java/com/jaredgood/sfgdi/SfgDiApplication.java
@@ -1,6 +1,8 @@
 package com.jaredgood.sfgdi;
 
 import com.jaredgood.sfgdi.controllers.*;
+import com.jaredgood.sfgdi.services.PrototypeBean;
+import com.jaredgood.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -35,6 +37,18 @@ public class SfgDiApplication {
 		PetController petController = ctx.getBean("petController", PetController.class);
 		System.out.println("--- The Best Pet is ---");
 		System.out.println(petController.whichPetIsTheBest());
+
+		System.out.println("---- Bean Scopes ---------");
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
+
 	}
 
 }
---
+++ src/main/java/com/jaredgood/sfgdi/services/SingletonBean.java
@@ -0,0 +1,13 @@
+package com.jaredgood.sfgdi.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SingletonBean {
+    public SingletonBean(){
+        System.out.println("Creating a Singleton bean");
+    }
+    public String getMyScope(){
+        return "I'm a Singleton";
+    }
+}
